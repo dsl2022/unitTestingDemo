ca89ee474f7a540d1a5c86587f67da24441f7694
JizongL <2015rpro@gmail.com>
2019-03-30 18:33:44 -0400

    added test suite

--- unit-testing/src/App.test.js
+++ unit-testing/src/App.test.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import renderer from 'react-test-renderer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders the UI as expected',()=> {
-  const tree = renderer
-  .create(<App message = 'testing'/>).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+describe('my app unit test',()=>{
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('renders the UI as expected',()=> {
+    const tree = renderer
+    .create(<App message = 'testing'/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
